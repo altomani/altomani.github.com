5e321707d8e5c834822eda4ad21889b2953e5188
Andrea Altomani <andrea.altomani@gmail.com>
2013-02-17 22:06:45 +0100

    with publications in yaml format

--- build/mkpublications.rb
+++ build/mkpublications.rb
@@ -2,6 +2,10 @@
 
 require 'rubygems'
 require 'bibtex'
+require 'psych'
 
 out = File.open("../publications.md", 'w')
-out.puts "---\nlayout: publications\ntitle: 'Publications'\npubl: 'active'\npubl_list: " + MultiJson.dump(BibTeX.open(ARGV[0]).to_citeproc) + "\n---\n\nsome text\n"
+bib = Psych.load(MultiJson.dump(BibTeX.open(ARGV[0]).to_citeproc))
+o = Hash["layout" => 'publications',"title" => 'Publications', "publ" => 'active', "publ_list" => bib ]
+puts o.to_yaml + "---\n\nNo text here"
+out.puts o.to_yaml + "---\n\nNo text here"
